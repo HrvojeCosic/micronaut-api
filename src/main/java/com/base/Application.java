52c07e8e58ccdd3e3e2bb30cb12f687d2df9a2d1
HrvojeCosic <hrvojecosic333@gmail.com>
2024-01-29 00:15:41 +0100

    Swagger: Generate petshelter.yml file

--- src/main/java/com/base/Application.java
+++ src/main/java/com/base/Application.java
@@ -2,6 +2,15 @@ package com.base;
 
 import io.micronaut.runtime.Micronaut;
 
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "petshelter"
+        ), servers = @Server(url = "https://localhost:8080/")
+)
 public class Application {
 
     public static void main(String[] args) {
